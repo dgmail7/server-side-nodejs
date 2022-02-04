e11a3453afcdcff6c99e8023cb843def9d233707
xx <xx>
2022-02-04 17:28:20 +0800

    open and handle error, DONOT check then use

--- node-http/index.js
+++ node-http/index.js
@@ -9,29 +9,39 @@ const server = http.createServer((req, res) => {
     console.log('Request for ' + req.url + ' by method ' + req.method)
 
     if (req.method == 'GET') {
+        // method GET
         var fileUrl;
         if (req.url == '/') fileUrl = '/index.html'
         else fileUrl = req.url
         var filePath = path.resolve('./public' + fileUrl)
         const fileExt = path.extname(filePath)
         if (fileExt == '.html') {
-            fs.exists(filePath, (exists) => {
-                if (!exists) {
-                    res.statusCode = 404
-                    res.setHeader('Content-Type', 'text/html')
-                    res.end('<html><body><h1>Error 404: ' + fileUrl + ' not found</h1></body></html>')
-                    return;
+            // html file
+            fs.open(filePath, 'r', (err, fd) => {
+                if (err) {
+                    if (err.code === 'ENOENT') {
+                        res.statusCode = 404
+                        res.setHeader('Content-Type', 'text/html')
+                        res.end('<html><body><h1>Error 404: ' + fileUrl + ' not found</h1></body></html>')
+                        return
+                    }
+                    throw err
                 }
                 res.statusCode = 200
                 res.setHeader('Content-Type', 'text/html')
-                fs.createReadStream(filePath).pipe(res);
+                // optional parameter 'emitClose' is true by default
+                // the stream will emit a 'close' event after it has been destroyed
+                // add fs.close(filePath) will cause error
+                fs.createReadStream('', {fd: fd}).pipe(res)
             })
         } else {
+            // not html file
             res.statusCode = 404
             res.setHeader('Content-Type', 'text/html')
             res.end('<html><body><h1>Error 404: ' + fileUrl + ' not an HTML file</h1></body></html>')
         }
     } else {
+        // not method GET
         res.statusCode = 404
         res.setHeader('Content-Type', 'text/html')
         res.end('<html><body><h1>Error 404: ' + req.method + ' not supported</h1></body></html>')
