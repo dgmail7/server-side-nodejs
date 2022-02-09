07e51043272cbf71b4b7938279cc11374dc48927
xx <xx>
2022-02-09 20:59:29 +0800

    Node MongoDB Example 2

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,5 +1,7 @@
 const MongoClient = require('mongodb').MongoClient
 const assert = require('assert')
+const { brotliDecompress } = require('zlib')
+const dboper = require('./operations')
 
 const url = 'mongodb://localhost:27017/'
 const dbname = 'conFusion'
@@ -9,28 +11,39 @@ MongoClient.connect(url, (err, client) => {
     console.log('Connected correctly to server')
 
     const db = client.db(dbname)
-    const collection = db.collection('dishes')
 
-    collection.insertOne({
-        "name": "Uthappizza",
-        "description": "test"
-    }, (err, result) => {
-        assert.equal(err, null)
+    dboper.insertDocument(
+        db,
+        {
+            name: 'Vadonut',
+            description: 'Test'
+        },
+        'dishes',
+        (result) => {
+            console.log('Insert Document:\n', result.ops)
 
-        console.log('\nAfter Insert:')
-        console.log(result.ops)
+            dboper.findDocuments(db, 'dishes', (docs) => {
+                console.log('Found Documents:\n', docs)
 
-        collection.find({}).toArray((err, docs) => {
-            assert.equal(err, null)
+                dboper.updateDocument(
+                    db,
+                    {name: 'Vadonut'},
+                    {description: 'Updated Test'},
+                    'dishes',
+                    (result) => {
+                        console.log('Updated Document:\n', result.result)
+                        dboper.findDocuments(db, 'dishes', (docs) => {
+                            console.log('Found Documents:\n', docs)
 
-            console.log('\nFound:')
-            console.log(docs)
+                            db.dropCollection('dishes', (result) => {
+                                console.log('Dropped Collection: ', result)
 
-            db.dropCollection('dishes', (err, result) => {
-                assert.equal(err, null)
-
-                client.close()
+                                client.close()
+                            })
+                        })
+                    }
+                )
             })
-        })
-    })
+        }
+    )
 })
